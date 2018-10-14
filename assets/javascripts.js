4ddbe82635a5f7281bcf7ad348d94ccafae946c5
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-10-14 19:20:45 +0100

    Basic Changes 2
    Still working on basic changes. trying to find the right navbar and animated hamburger

--- assets/javascripts.js
+++ assets/javascripts.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
 });
 
-$('.button-collapse').sideNav({
-  menuWidth: 300, // Default is 300
-  edge: 'left', // Choose the horizontal origin
-  closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  draggable: true // Choose whether you can drag to open on touch screens
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
 });
