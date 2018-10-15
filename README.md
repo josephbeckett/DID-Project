110ac2dc28dcda66189bdc5837cbfe8c0bdbf257
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-10-15 11:27:49 +0100

    Changes 5
    Edited the read me file, and changed a few things back to html to edit before going php for live version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DID Project
+
+This Github repository is for my second year HND project Title "The Final Frontier". It is a Digital Interactive Design website built to educate people about space and the universe. It will involve a quiz and leaderboard system which will be accessed by logging into the website reading through the resources provided and answering questions at the end, to test your knowledge and understanding to make sure that you've actually read through the site and retained the information on it.
+
+Thank you for looking at my project and I hope you enjoy it.
---
+++ basewebsite/normalnavbar.php
@@ -0,0 +1,15 @@
+<nav class="transparentBG z-depth-0">
+  <div class="nav-wrapper">
+      <a href="#" data-activates="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="hide-on-med-and-down left">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
+      </ul>
+      <ul class="sidenav" id="mobile-demo">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="signup.php">Sign Up</a></li>
+          <li><a href="login.php">Login</a></li>
+      </ul>
+  </div>
+</nav>
