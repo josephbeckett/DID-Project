bf65f8f8fecec3f7c95f2243ca4f092fb6d87dcc
JosephBeckett <joseph.beckett1999@gmail.com>
2018-10-15 08:06:19 +0100

    Basic Changes 3

--- assets/javascripts.js
+++ assets/javascripts.js
@@ -3,12 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems, options);
 });
 
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-// Or with jQuery
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
+});
+
+// Index Page Loader
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(2000).fadeOut('slow');
+
+	$('.preloader-wrapper')
+		.delay(5000)
+		.fadeOut();
 });
